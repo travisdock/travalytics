7a42d0976773fa950f6a027c1f4af329c4fe6743
Travis Dockter <travisdock@gmail.com>
2025-07-17 22:56:36 -0700

    Make dashboard better

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,49 +1,33 @@
 class DashboardsController < ApplicationController
   before_action :require_authentication
-  before_action :set_site
 
   def show
     @sites = current_user.sites
 
-    if @site
-      @total_events = @site.events.humans_only.count
-      @page_views = @site.events.humans_only.page_views.count
-      @bot_traffic = @site.events.bots_only.count
-      @popular_pages = @site.events.humans_only.page_views
-                            .group("properties->>'path'")
-                            .count
-                            .sort_by { |_, count| -count }
-                            .first(10)
-      @recent_events = @site.events.humans_only.recent.limit(50)
-    else
-      @total_events = 0
-      @page_views = 0
-      @bot_traffic = 0
-      @popular_pages = []
-      @recent_events = []
+    # Build stats for each site
+    @sites_with_stats = @sites.map do |site|
+      {
+        site: site,
+        total_events: site.events.humans_only.count,
+        page_views: site.events.humans_only.page_views.count,
+        bot_traffic: site.events.bots_only.count,
+        popular_pages: site.events.humans_only.page_views
+                          .group("properties->>'path'")
+                          .count
+                          .sort_by { |_, count| -count }
+                          .first(5) # Show top 5 pages per site
+      }
     end
   end
 
   def events
-    if @site
-      @events = @site.events.humans_only
-                     .by_date_range(filter_start_date, filter_end_date)
-                     .recent
-      # Note: pagination would need to be added with a gem like kaminari or pagy
-    else
-      @events = []
-    end
+    @site = current_user.sites.find(params[:site_id])
+    @events = @site.events.humans_only
+                   .by_date_range(filter_start_date, filter_end_date)
+                   .recent
   end
 
   private
-
-  def set_site
-    @site = if params[:site_id]
-      current_user.sites.find_by(id: params[:site_id])
-    else
-      current_user.sites.first
-    end
-  end
 
   def filter_start_date
     params[:start_date]&.to_date || 7.days.ago
