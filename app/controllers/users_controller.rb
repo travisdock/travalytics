2c888bb2736172aadb8372bd2a91feda6a2ae63f
Travis <36681963+travisdock@users.noreply.github.com>
2025-08-07 19:20:37 -0600

    Disable signup and forgot password pages for now (#22)
    * Disable all the signup and forgot password stuff for now
    
    * Keep old code for reference

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,22 @@ class UsersController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
 
   def new
-    @user = User.new
+    # Temporarily disable signups
+    redirect_to root_path, alert: "Sorry, new user registration is temporarily disabled. Please check back later."
   end
 
   def create
-    @user = User.new(user_params)
+    # @user = User.new(user_params)
 
-    if @user.save
-      start_new_session_for @user
-      redirect_to after_authentication_url, notice: "Welcome! Your account has been created."
-    else
-      render :new, status: :unprocessable_entity
-    end
+    # if @user.save
+    #   start_new_session_for @user
+    #   redirect_to after_authentication_url, notice: "Welcome! Your account has been created."
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+
+    # Temporarily disable signups
+    redirect_to root_path, alert: "Sorry, new user registration is temporarily disabled. Please check back later."
   end
 
   private
