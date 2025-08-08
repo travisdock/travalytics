378fb9de4d64fd6966d3b6cecc3b145a948617a5
Travis Dockter <travisdock@gmail.com>
2025-08-07 19:17:11 -0600

    Keep old code for reference

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    # @user = User.new(user_params)
+
+    # if @user.save
+    #   start_new_session_for @user
+    #   redirect_to after_authentication_url, notice: "Welcome! Your account has been created."
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+
     # Temporarily disable signups
     redirect_to root_path, alert: "Sorry, new user registration is temporarily disabled. Please check back later."
   end
